663f243c3ec95325097f4a59e373a2c4933c2f35
filipag <filipa.isabel.goncalves@gmail.com>
2014-10-09 10:57:19 +0100

    second version

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,6 +5,6 @@ public class HelloWorld {
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello World");
-		System.out.println("1st version");
+		System.out.println("2st version");
 	}
 }
